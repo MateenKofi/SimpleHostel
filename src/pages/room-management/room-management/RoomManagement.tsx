d46896dec48412fab2baafef2a78466ea6f3f6dc
Abdul Mateen Kofi Yeboah <mateenabdullopato@gmail.com>
2025-02-03 23:42:15 +0000

    feat: enhance RoomManagement component with no rooms found message and improved data handling

--- src/pages/room-management/room-management/RoomManagement.tsx
+++ src/pages/room-management/room-management/RoomManagement.tsx
@@ -69,20 +69,20 @@ const RoomManagement = () => {
               Authorization: `Bearer ${localStorage.getItem("token")}`,
             },
           });
-          return response?.data.data.rooms;
+          return response?.data.data;
         },
     });
-    const { data: amenitiesData, isLoading: isAmenitiesLoading, isError: isAmenitiesError } = useQuery<{ data: { id: string; name: string; price: number; }[] }>({
-        queryKey: ["amenities"],
-        queryFn: async () => {
-          const response = await axios.get(`/api/amenities/hostel/${hostelId}`, {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("token")}`,
-            },
-          });
-          return response?.data;
-        },
-    });
+    // const { data: amenitiesData, isLoading: isAmenitiesLoading, isError: isAmenitiesError } = useQuery<{ data: { id: string; name: string; price: number; }[] }>({
+    //     queryKey: ["amenities"],
+    //     queryFn: async () => {
+    //       const response = await axios.get(`/api/amenities/hostel/${hostelId}`, {
+    //         headers: {
+    //           Authorization: `Bearer ${localStorage.getItem("token")}`,
+    //         },
+    //       });
+    //       return response?.data;
+    //     },
+    // });
 
     return (
         <div className="p-6">
@@ -115,8 +115,20 @@ const RoomManagement = () => {
 
             {/* Stats Overview */}
          
-
-            <div className="bg-white rounded-lg shadow-sm p-4">
+            {data?.rooms?.length === 0 ? (
+                 <div className="w-full flex justify-center flex-col items-center gap-4 text-center py-4 mt-20">
+                 <p>No rooms found. Please add some rooms.</p>
+                 <div>
+                 <button className="flex gap-2 px-4 py-2 bg-black text-white rounded-md"
+                        onClick={() => openAddRoomModal()}
+                    >
+                        <Plus />
+                        <span>Room</span>
+                    </button>
+                 </div>
+             </div>
+            ):(
+                <div className="bg-white rounded-lg shadow-sm p-4">
                 <div className="flex justify-between items-center mb-4">
                     <div className="relative">
                         <Search className="w-5 h-5 absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
@@ -133,13 +145,16 @@ const RoomManagement = () => {
                         </button>
                     </div>
                 </div>
-
                 <DataTable
                     columns={columns}
-                    data={data || []}
-                    pagination
+                    data={data?.rooms || []}
+                    pagination 
+                    highlightOnHover
+                    responsive
                 />
             </div>
+            )}
+           
         </div>
     );
 };
