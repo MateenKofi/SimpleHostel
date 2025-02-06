008f7fb65df5e4eace76ad70ca8d4437d4611470
Abdul Mateen Kofi Yeboah <mateenabdullopato@gmail.com>
2025-02-06 11:11:15 +0000

    feat: add loading skeleton for RoomManagement component to enhance user experience

--- src/pages/room-management/room-management/RoomManagement.tsx
+++ src/pages/room-management/room-management/RoomManagement.tsx
@@ -123,6 +123,18 @@ const RoomManagement = () => {
   if (isLoading) {
     return (
       <div className="p-6">
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 my-3">
+          {[0, 1, 2, 3].map((_, index) => (
+            <div key={index} className="bg-gray-200 rounded-lg shadow-sm p-4">
+             <div className="flex items-center gap-2 mb-4">
+             <div className="w-6 h-6 bg-gray-300 rounded-full animate-pulse"></div>
+             <div className="h-6 bg-gray-300 rounded-md w-32 animate-pulse"></div>
+             </div>
+             <div className="h-14 w-14 bg-gray-300 rounded-md animate-pulse"></div>
+             <div className="h-6 w-full bg-gray-300 rounded-md animate-pulse mt-1"></div>
+            </div>
+          ))}
+        </div>
         <div className="flex justify-between items-center mb-6">
           <div className="flex items-center gap-2">
             <div className="w-6 h-6 bg-gray-300 rounded-full animate-pulse"></div>
