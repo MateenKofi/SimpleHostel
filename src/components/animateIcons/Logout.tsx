95cbd5abf13c2e4e58ecec0709935590462278bc
Abdul Mateen Kofi Yeboah <mateenabdullopato@gmail.com>
2025-02-26 01:38:15 +0000

    style: enhance LogoutIcon styling for improved hover effects and responsiveness

--- src/components/animateIcons/Logout.tsx
+++ src/components/animateIcons/Logout.tsx
@@ -65,7 +65,9 @@ const LogoutIcon = forwardRef<LogoutIconHandle, LogoutIconProps>(
     return (
       <div
         className={cn(
-          `cursor-pointer select-none  hover:black rounded-md transition-colors duration-200 flex items-center justify-center gap-2`,
+          `w-full cursor-pointer select-none  hover:bg-black rounded-md  duration-300 flex items-center  gap-2 py-2 text-sm text-gray-700 hover:text-white  px-2 
+          transition-all ease-in-out
+          `,
           className
         )}
         onMouseEnter={handleMouseEnter}
