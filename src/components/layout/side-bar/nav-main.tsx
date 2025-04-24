e2591b49a97b6c7b908e66890ce3fdfc593c5e8a
Abdul Mateen Kofi Yeboah <mateenabdullopato@gmail.com>
2025-04-24 04:29:06 +0000

    feat: refactor NavMain component to streamline navigation item management based on user roles

--- src/components/layout/side-bar/nav-main.tsx
+++ src/components/layout/side-bar/nav-main.tsx
@@ -19,34 +19,46 @@ import {
   Calendar,
   UserCog,
   Wallet2,
+  LucideProps,
 } from "lucide-react"
-
+ 
+type navItems = {
+  title: string;
+  icon: React.ComponentType<LucideProps>;
+  path: string;
+}
 export function NavMain() {
   const userRole = localStorage.getItem('role')
   const navigate = useNavigate();
 
-  const navMain = [
-    { title: "Dashboard", icon: LayoutDashboard, path: "/dashboard" },
-    { title: "Calendar Year", icon: Calendar, path: "/dashboard/calendar-year-management" },
-    { title: "Room Management", icon: BedDouble, path: "/dashboard/room-management" },
-    { title: "Resident Management", icon: BookOpenCheck, path: "/dashboard/resident-management" },
-    { title: "Debtors List", icon: List, path: "/dashboard/debtors-list" },
-    { title: "Visitor Management", icon: CalendarCheck, path: "/dashboard/visitor-management" },
-    { title: "Maintenance & Tracking", icon: Wrench, path: "/dashboard/maintenance-and-tracking" },
-    { title: "Staff Management", icon: Users, path: "/dashboard/staff-management" },
-    {title:"Transactions", icon:Wallet2 ,path:'/dashboard/transactions'}
-  ]
+  let navItems:navItems[] = [];
 
-  if (userRole === "SUPER_ADMIN") {
-    navMain.push({ title: "Approve Hostel", icon: CheckSquare, path: "/dashboard/approve-hostel" })
-    navMain.push({ title: "Users", icon: UserCog, path: "/dashboard/users" })
+  if (userRole === 'ADMIN') {
+    navItems = [
+      { title: "Dashboard", icon: LayoutDashboard, path: "/dashboard" },
+      { title: "Calendar Year", icon: Calendar, path: "/dashboard/calendar-year-management" },
+      { title: "Room Management", icon: BedDouble, path: "/dashboard/room-management" },
+      { title: "Resident Management", icon: BookOpenCheck, path: "/dashboard/resident-management" },
+      { title: "Debtors List", icon: List, path: "/dashboard/debtors-list" },
+      { title: "Visitor Management", icon: CalendarCheck, path: "/dashboard/visitor-management" },
+      { title: "Maintenance & Tracking", icon: Wrench, path: "/dashboard/maintenance-and-tracking" },
+      { title: "Staff Management", icon: Users, path: "/dashboard/staff-management" },
+      { title: "Transactions", icon: Wallet2, path: '/dashboard/transactions' }
+    ];
+  } else if (userRole === "SUPER_ADMIN") {
+    navItems = [
+      { title: "Dashboard", icon: LayoutDashboard, path: "/dashboard" },
+      { title: "Approve Hostel", icon: CheckSquare, path: "/dashboard/approve-hostel" },
+      { title: "Transactions", icon: Wallet2, path: '/dashboard/transactions' },
+      { title: "Users", icon: UserCog, path: "/dashboard/users" }
+    ];
   }
 
   return (
     <SidebarGroup>
       <SidebarGroupLabel>Navigation</SidebarGroupLabel>
       <SidebarMenu>
-        {navMain.map((item) => (
+        {navItems.map((item) => (
           <SidebarMenuItem key={item.title}>
             <SidebarMenuButton
               onClick={() => navigate(item.path)}
